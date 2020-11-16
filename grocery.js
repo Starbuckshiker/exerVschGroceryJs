3ccc3fd268b74ccb5a57a45139cadcc11e0dda0e
starbuckshiker <u1304954@umail.utah.edu>
2020-11-16 13:42:31 -0700

    shopper

--- grocery.js
+++ grocery.js
@@ -3,5 +3,10 @@ var shopper = {
     age: 59,
     member: true,
     groceryCart: ["milk", "eggs", "bread", "cheese", "apples"],
-    }
+     }
 console.log(shopper)
+function getShopper(shopper) {
+    for (var i = 0; i < shopper.length; i++) {
+    console.log(shopper[i].name + " is " + shopper[i].age + " years old!");
+  }
+}
